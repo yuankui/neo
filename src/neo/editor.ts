f89696a85f675754e430887e9ff629812ed361fb
yuankui <yuankui@meituan.com>
2020-04-20 17:04:51 +0800

    up

--- src/neo/editor.ts
+++ src/neo/editor.ts
@@ -1,8 +1,13 @@
-import {HookRegister} from "neo-hooks";
+import {HookRegister, HookRegisterConsumer} from "neo-hooks";
 
 export class Editor {
     register: HookRegister;
-    constructor() {
+    constructor(consumers?: Array<HookRegisterConsumer>) {
         this.register = new HookRegister();
+        if (consumers) {
+            consumers.forEach(c => {
+                c.init(this.register);
+            })
+        }
     }
 }
